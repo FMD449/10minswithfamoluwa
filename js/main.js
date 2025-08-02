6b9e0263bb7b20025525f6491d31bde00be387f8
FMD449 <Famoluwagbemi2019@gmail.com>
2025-08-02 01:25:21 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+10minswithfamoluwa/   ← your GitHub repo
+├── index.html
+├── about.html
+├── episodes.html
+├── contact.html
+│
+├── css/
+│   └── styles.css
+│
+├── js/
+│   └── main.js
+│
+├── images/
+│   ├── logo.png
+│   ├── background.jpg
+│   └── famoluwa.jpg
+│
+└── audio/
+    ├── episode1.mp3
+    └── episode2.mp3
